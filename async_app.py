cbc9f8b52db34751a7e6985ea5976f398f63e0dc
unknown <ykt_andrey@mail.ru>
2023-11-11 15:51:28 +0300

    fix internal error in ml model and add different loggers for errors

--- async_app.py
+++ async_app.py
@@ -1,24 +1,26 @@
 import uvicorn
-import captcha_report
+# import captcha_report
 import microservice
 
 from contextlib import asynccontextmanager
-from captcha_report.database.utils.create_tables import create_captcha_report_tables
+# from captcha_report.database.utils.create_tables import create_captcha_report_tables
 from microservice.exceptions.fastapi_exception_handler import FastApiExceptionHandler
 from fastapi import FastAPI
 from kink import inject
 
 
-@inject
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await create_captcha_report_tables()
-    yield
+# @inject
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     await create_captcha_report_tables()
+#     yield
 
 
-app = FastAPI(lifespan=lifespan)
+# app = FastAPI(lifespan=lifespan)
+
+app = FastAPI()
 app.add_exception_handler(Exception, FastApiExceptionHandler())
-app.include_router(captcha_report.router)
+# app.include_router(captcha_report.router)
 app.include_router(microservice.router)
 
 
